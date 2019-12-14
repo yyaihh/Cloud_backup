5e91bf5ae56f5224a790d56dcd77765f1b2ece63
yyaihh <1903870154@qq.com>
2019-12-14 19:22:51 +0800

    提交

---
+++ IPC/pipe_fork.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+int main(){
+    int pfd[2];
+    if(pipe(pfd) < 0){
+        perror("make pipe");
+        exit(1);
+    }
+    pid_t pid = fork();
+    if(pid < 0){
+        perror("fork");
+    }
+    else if(pid == 0){
+        char buf[100];
+        int len;
+        close(pfd[0]);
+        while(fgets(buf, 100, stdin)){
+            len = strlen(buf);
+            printf("子进程将所输字符串写入管道\n");
+            if(write(pfd[1], buf, len) != len){
+                perror("write to pipe");
+            } 
+            usleep(100);
+        }
+    }
+    else{
+        char buf[100];
+        int len;
+        close(pfd[1]);
+        while(1){
+            usleep(100);
+            if((len = read(pfd[0], buf, 100)) == -1){
+               perror("read from pipe");
+            }  
+            write(1, buf, len);
+            printf("父进程从管道读出字符串, 打印到屏幕\n");
+        }
+    }
+    exit(0);
+}
