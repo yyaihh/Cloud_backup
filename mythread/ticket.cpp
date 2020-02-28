4c548707d6bb8df9d37483da1186df909d563c72
yyaihh <test@localhost.localdomain>
2020-02-29 00:10:39 +0800

    提交

--- mythread/ticket.cpp
+++ mythread/ticket.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
+#include<cstdio>
 #include<unistd.h>
 #include<pthread.h>
 using namespace std;
+
 int ticket = 100;
+
 void* route(void *arg){
     int n = *(int*)arg;
     while(1){
         if(ticket > 0){
-            usleep(1000);
-            cout << "第"<< n <<"个黄牛抢到票, 还剩"<< ticket << endl;
+            usleep(100000);
+            printf("第%d个黄牛抢到票, 还剩%d张\n", n, ticket);
             --ticket;
         }
         else{
@@ -20,10 +23,13 @@ void* route(void *arg){
 int main(){
     pthread_t tid[4];
     for(int i = 0; i < 4; ++i){
-        int ret = pthread_create(tid + i, NULL, route, &i);
+        int ret = pthread_create(&tid[i], NULL, route, &i);
         if(ret > 0){
             cout << "创建进程失败\n";
         }
+    }
+    for(int i = 0; i < 4; ++i){
+        pthread_join(tid[i], NULL);
     }
     return 0;
 }
